3824dd9e2a4db3480f76e25f69bfb0cf8bb48b00
MawranMashra <47461193+MarwanMashra@users.noreply.github.com>
2021-02-23 23:56:48 +0100

    utilisation de cv2 dans un docker

--- emotion_recognition/app.py
+++ emotion_recognition/app.py
@@ -13,10 +13,10 @@ def run_detection_path(path):
 
 if __name__ == '__main__':
 
-    # read_emotion("C:/xampp/htdocs/FDS/S6/HLIN601/TER_S6/.tmp/emotion.txt")
+    read_emotion("C:/xampp/htdocs/FDS/S6/HLIN601/TER_S6/.tmp/emotion.txt")
     # run_realtime_emotions()
 
-    if(len(sys.argv)>2 and sys.argv[2].isdigit()):
-        read_emotion(sys.argv[1],int(sys.argv[2]))
-    else:
-        read_emotion(sys.argv[1])
+    # if(len(sys.argv)>2 and sys.argv[2].isdigit()):
+    #     read_emotion(sys.argv[1],int(sys.argv[2]))
+    # else:
+    #     read_emotion(sys.argv[1])

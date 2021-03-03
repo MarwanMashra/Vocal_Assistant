176adae6e4ec1ed96e6212ec485f80ffa5f6ec89
MawranMashra <47461193+MarwanMashra@users.noreply.github.com>
2021-03-03 21:05:10 +0100

    ajout de test

---
+++ test-voix/app.py
@@ -0,0 +1,17 @@
+from gtts import gTTS 
+import os
+from playsound import playsound
+
+def convert_audio(text):
+        
+    language = 'fr'
+    
+    myobj = gTTS(text=text, lang=language, slow=False)
+    
+    myobj.save("audio.mp3") 
+    
+    playsound("audio.mp3")
+
+    os.remove("audio.mp3")
+
+convert_audio("salut")

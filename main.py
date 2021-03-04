338f9ec6d0e9fc347326075c5b941b0d169e48bb
MawranMashra <47461193+MarwanMashra@users.noreply.github.com>
2021-03-04 05:37:33 +0100

    MAJ main.py, attendre le résultat de la détection des émotions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os,sys
+import os,sys,time
 from os.path import dirname
 from webcam import Webcam
 import docker
@@ -6,11 +6,29 @@ import docker
 
 if __name__ == '__main__':
     
+    data_path= os.getcwd()+"/_data"
     Webcam.open()
-    Webcam.take_photo(os.getcwd()+"/_data")
+    Webcam.take_photo(data_path)
     Webcam.close()
 
-    volumes={os.getcwd()+"/_data":{'bind': '/volume', 'mode': 'rw'}}
+    t=time.time()
+    wait=10
+    volumes={str(data_path):{'bind': '/volume', 'mode': 'rw'}}
     
     client = docker.from_env()
     client.containers.run('ter_s6_emotion_recognition',command='volume/face.jpg volume/emotion.txt',volumes=volumes)
+
+    while True:   # wait for the result of emotion_recognition
+
+        # result came out 
+        if (os.stat(data_path+"/emotion.txt")[8] >= t ):   
+            f= open(data_path+"/emotion.txt","r")
+            print("Emotion: "+f.readline())
+            f.close()
+            break
+
+        # time out and no result yet
+        if (time.time() > t+wait):
+            print("Timeout, no emotion has been detected")
+            break
+
